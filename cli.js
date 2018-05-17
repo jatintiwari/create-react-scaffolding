ccea8ee56614c1d9e1b08be8d4ad657f33f4d9f3
jatin-tiwari <jatin.tiwari@flipkart.com>
2018-05-17 16:03:41 +0530

    ask questions

--- cli.js
+++ cli.js
@@ -1,15 +1,37 @@
 #!/usr/bin/env node
 "use strict";
-
+/**
+ * dependencies
+ */
 const program = require('commander');
+const packageJSON = require('./package');
+const { prompt } = require('inquirer');
+
+/**
+ * constants
+ */
+const version = packageJSON.version;
+const name = packageJSON.name;
+
+const questionsBeforeAdd = [{
+  type: 'input',
+  name: 'name',
+  message: 'Please enter name   => '
+},{
+  type: 'input',
+  name: 'contact',
+  message: 'Please enter contact   => '
+}]
 
 const app = {
   contacts: {
     "000": 'hulu lulu'
   },
-  addContact: function (name = "", contact = "000") {
-    console.info('Adding contact: ', name, contact);
-    return this.contacts[contact] = name;
+  addContact: function () {
+    return prompt(questionsBeforeAdd).then((answer) => {
+      console.info('Adding contact: ', answer);
+      return this.contacts[answer.contact] = answer.name;
+    })
   },
   getContact: function (contact) {
     const foundContact = this.contacts[contact] ? this.contacts[contact] : 'none';
@@ -20,11 +42,11 @@ const app = {
 
 
 program
-  .version('0.0.1')
-  .description('Contact management system');
+  .version(version)
+  .description(name);
 
 program
-  .command('addContact <name> <contact>')
+  .command('addContact')
   .alias('a')
   .description('Add a contact')
   .action(app.addContact.bind(app));
